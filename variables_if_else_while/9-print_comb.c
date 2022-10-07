c755e1357b393c7792e0e716d7ecfd884184db88
Bryan <kotrobryan@gmail.com>
2022-10-07 03:50:20 -0700

    Numbers,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - prints all numbers from 0 to 9 with comas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
