d5f70c5fbce6ee0c4324f5394074e859d8e1f19c
Bryan <kotrobryan@gmail.com>
2022-10-07 02:58:25 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Main - Prints number from 1 to 10 .
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
