d24dc106dbf62fe1acd679c21dd25be6755d0560
Bryan <kotrobryan@gmail.com>
2022-10-18 03:01:53 -0700

    Puts recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: Pointer
+ *
+ */
+void _puts_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s != '\0')
+	{
+		n++;
+		putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else if (*s == s[n])
+		putchar('\n');
+}
