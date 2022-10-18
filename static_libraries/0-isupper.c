c50a4b93fa2db210b91d7fd829ec379394be99af
Bryan <kotrobryan@gmail.com>
2022-10-18 13:17:26 -0700

    Static Libraries

--- static_libraries/0-isupper.c
+++ static_libraries/0-isupper.c
@@ -1,3 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
-int _isupper(int c);
+/**
+ * _isupper - Entry point
+ * @c: character
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
