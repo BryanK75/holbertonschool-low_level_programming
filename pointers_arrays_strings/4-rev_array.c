4608a1242a008413a6d011341d2543b1b3123a3b
Bryan <kotrobryan@gmail.com>
2022-10-19 12:01:28 -0700

    reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: Pointer
+ * @n: Integer
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	n = n - 1;
+
+	for (i = 0; i < n; i++, n--)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
+}
