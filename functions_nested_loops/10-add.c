0c46c829b1f4d1178c8e3b8232f4491c5b193078
Bryan <kotrobryan@gmail.com>
2022-10-19 05:52:49 -0700

    Addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * add - Entry point
+ * @a: number
+ * @b: number
+ *
+ * Return: Number (Success)
+ */
+int add(int a, int b)
+{
+	int res = a + b;
+	
+	return (res);
+	_putchar('\n');
+}
