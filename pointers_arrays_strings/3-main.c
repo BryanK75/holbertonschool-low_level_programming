b78ac343992cbee092a231e383981af87d3fd7d1
Bryan <kotrobryan@gmail.com>
2022-10-19 13:23:57 -0700

    strspn

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,19 +1,5 @@
 #include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- *
-int main(void)
-{
-	char *str;
-
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	_puts(str);
-	return (0);
-}
-*/
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -22,11 +8,11 @@ int main(void)
  */
 int main(void)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
 }
