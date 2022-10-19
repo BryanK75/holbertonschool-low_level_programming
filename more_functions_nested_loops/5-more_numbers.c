1dd5d744032b260ee6ddeddcdec75bc3c12514dc
Bryan <kotrobryan@gmail.com>
2022-10-19 08:21:30 -0700

    More numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  * more_numbers - Entry point
+ *   *
+ *    */
+void more_numbers(void)
+{
+	int index = 0;
+
+	while (index < 10)
+	{
+		int i;
+
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('0' + (i / 10));
+			}
+			_putchar('0' + (i % 10));
+
+		}
+		_putchar('\n');
+		index++;
+	}
+}
