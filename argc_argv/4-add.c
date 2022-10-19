69176bee6e59b1506c90c79616c77d2bec2bf03e
Bryan <kotrobryan@gmail.com>
2022-10-19 04:32:17 -0700

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,31 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-/**
- * _isdigit - Entry point
- * @c: counts arguments
- * @s: array of arguments
- *
- * Return: 1 if error,or 0 (Success)
- */
-int _isdigit(int c, char *s)
-{
-	int i, res;
-
-	for (i = 0; i < c; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			res = 1;
-		else
-			res = 0;
-	}
-	if (res == 1)
-		return (1);
-	else
-		return (0);
-}
-
 /**
  * main - adds positive numbers
  * @argc: Integer
@@ -36,27 +11,25 @@ int _isdigit(int c, char *s)
  */
 int main (int argc, char *argv[])
 {
-	int i, sum, j, n = 0;
+	int i, j, r = 0;
 
-	if (argc <= 1)
+	if (argc < 1)
+	{
 		printf("O\n");
-	for (j = 1; j < argc; j++)
-	{
-		n = strlen(argv[j]);
-		if (_isdigit(n, argv[j]) == 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
+		return (0);
 	}
-	if (argc >= 2)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j=0; argv[i][j] != '\0'; j++)
 		{
-			sum += atoi(argv[i]);
+			if (argv[i][j] < 'z' && argv[i][j] > 'a')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		printf("%d\n", sum);
+		r += atoi(argv[i]);
 	}
+	printf("%d\n", r);
 	return (0);
 }
-
