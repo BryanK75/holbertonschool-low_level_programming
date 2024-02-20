5442f53c65c99f3eef179987b22e9191f7e3e7e1
BryanK75 <kotrobryan@gmail.com>
2024-02-20 09:49:25 +0000

    Alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet without the letters e and q
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	{
+		for (n = 97; n < 123; n++)
+		{
+			if (n != 101 && n != 113)
+			{
+				putchar (n);
+			}
+		}
+		putchar ('\n');
+		return (0);
+	}
+}
+
