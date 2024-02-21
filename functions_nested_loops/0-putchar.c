9f4d3de2d572cd16efd4d274fd90d894403e2b91
BryanK75 <kotrobryan@gmail.com>
2024-02-21 03:30:24 -0800

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints _putchar followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	char c[8] = "_putchar";
+	{
+		for (i = 0; i < 8; i++)
+		{
+			_putchar(c[i]);
+		}
+		_putchar('\n');
+		return (0);
+	}
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Those are my first steps into functions and nested loops in C.
