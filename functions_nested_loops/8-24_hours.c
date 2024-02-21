149ed6045b6a25edfe802259f1ddf86430db9c04
BryanK75 <kotrobryan@gmail.com>
2024-02-21 08:57:26 -0800

    Hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,26 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int h1, h2, min1, min2; 
-
-	for (h1 = 48; h1 <= 50; h1++)
+	int h, min;
+	for (h = 0; h <= 23; h++)
 	{
-		for (h2 = 48; h2 <= 51; h2++)
+		for (min = 0; min <= 59; min++)
 		{
-			for (min1 = 48; min1 <= 53; min1++)
-			{
-				for (min2 = 48; min2 <= 57; min2++)
-				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(58);
-					_putchar(min1);
-					_putchar(min2);
-					_putchar('\n');
-					if (h1 == 50 && h2 == 51 && min1 == 53 && min2 == 57)
-						return;
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(58);
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
+
