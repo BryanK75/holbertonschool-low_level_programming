335f8df1d0fcc7595bf701f498f5eabb7dc034c5
BryanK75 <kotrobryan@gmail.com>
2024-03-07 10:45:39 +0000

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring
+ * 
+ * @haystack: character
+ * @needle: character
+ *
+ * Return: character
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *n = needle;
+		char *h = haystack;
+
+		while (*h && *n && *h == *n)
+		{
+			n++;
+			h++;
+		}
+		if (!*n)
+			return (haystack);
+		haystack++;
+	}
+	return ('\0');
+}
