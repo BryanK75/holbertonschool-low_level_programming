b516708107c4592c67dfbeae28b84a5f508cfb7a
BryanK75 <kotrobryan@gmail.com>
2024-03-07 10:21:00 +0000

    strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - function that gets the length of prefix substring
+ *
+ * @s : character
+ * @accept: character
+ *
+ * Return : unsigned integer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int sum = 0;
+	int found = 0;
+
+	while (*s)
+	{
+		char *ptr = accept;
+
+		found = 0;
+		while (*ptr)
+		{
+			if (*s == *ptr)
+			{
+				found = 1;
+				break;
+			}
+			ptr++;
+		}
+		if (found == 1)
+			sum++;
+		else
+			break;
+		s++;
+	}
+	return(sum);
+}
