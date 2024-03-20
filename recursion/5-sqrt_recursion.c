28420b96450f44eed28a14a36872f6f58d60e6c5
BryanK75 <kotrobryan@gmail.com>
2024-03-20 23:55:18 +0000

    is prime

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,8 +2,6 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  *
- * @n: integer
- *
  * Return: integer
  */
 /**
@@ -19,24 +17,29 @@ int square_root_recursive(int n, int start, int end)
 {
 	int mid = (start + end) / 2;
 	int square = mid * mid;
-	
+
 	if (start > end)
 		return (-1);
 
 	if (square == n)
-        	return (mid);
+		return (mid);
 	else if (square < n)
-        	return (square_root_recursive(n, mid + 1, end));
+		return (square_root_recursive(n, mid + 1, end));
 	else
 		return (square_root_recursive(n, start, mid - 1));
-}
-
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ *
+ * @n: integer.
+ *
+ * Return: integer
+ */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
-    else if (n == 0 || n == 1)
-        return (-1);
-    else
-        return (square_root_recursive(n, 1, n));
+	if (n < 0)
+		return -1;
+	else if (n == 0 || n == 1)
+		return (-1);
+	else
+		return (square_root_recursive(n, 1, n));
 }
