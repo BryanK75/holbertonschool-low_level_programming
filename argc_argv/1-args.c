b25736256b9de131a925f3be823213661a79d671
BryanK75 <kotrobryan@gmail.com>
2024-03-21 00:21:00 +0000

    argc argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: integer
+ * @argv: string
+ * Return: integer
+ * **/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
