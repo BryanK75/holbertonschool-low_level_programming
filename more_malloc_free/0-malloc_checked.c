8716384380718b532833b6c2faa13f72af6d9842
BryanK75 <kotrobryan@gmail.com>
2024-04-30 09:53:37 +0000

    fixed betty coding style

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,11 +3,15 @@
 #include <stdlib.h>
 #include <limits.h>
 
-void *malloc_checked(unsigned int b) {
-    void *ptr = malloc(b);
-    if (ptr == NULL) {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(98);
-    }
-    return ptr;
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(98);
+	}
+	return (ptr);
 }
