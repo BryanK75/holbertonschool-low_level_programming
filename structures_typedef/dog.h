37170dcc70ac680569b879bd75cb7480e83b018c
BryanK75 <kotrobryan@gmail.com>
2024-05-01 09:41:38 +0000

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/**
+ * struct dog - Dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Description: Structure of a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+#endif
