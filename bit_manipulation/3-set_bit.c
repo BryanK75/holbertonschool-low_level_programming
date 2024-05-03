284fea6c6c1bed42a833af1a69b431a6d3b22fdf
BryanK75 <kotrobryan@gmail.com>
2024-05-03 08:10:41 +0000

    fixed doc task 3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * get_bit - Return the value of a bit at given index
+ * set_bit - Set the value of a bit to 1 at given index
  * @n: Number
  * @index: Index
- * Return: Value of bit at index or -1
+ * Return: 1 or -1
  */
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	return ((n >> index) & 1);
+	*n |= (1 << index);
+	return (1);
 }
